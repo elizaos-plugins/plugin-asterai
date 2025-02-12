94534ff474ef272a0370ccba25f5f7b0be53ae7c
Carlos V <carlosv.provoos@gmail.com>
2025-02-12 11:12:36 -0500

    chore: update plugin dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,12 @@
+import * as _elizaos_core from '@elizaos/core';
 import { AsteraiClient } from '@asterai/client';
 
 declare const getInitAsteraiClient: (agentId: string, publicQueryKey: string) => AsteraiClient;
 declare const asteraiPlugin: {
     name: string;
     description: string;
-    providers: Provider[];
-    actions: Action[];
+    providers: _elizaos_core.Provider[];
+    actions: _elizaos_core.Action[];
     evaluators: any[];
     services: any[];
 };
